55985c3f81c31fd576b7ffc5c495a8351329ad14
jlopezmUIA <jlopezm@uia.ac.cr>
2023-06-16 09:24:44 -0600

    Cambios

--- TicketsControl/tickets/impresion.py
+++ TicketsControl/tickets/impresion.py
@@ -9,37 +9,37 @@ def imprimir(codigo, departamento):
     fecha_actual = date.today().strftime('%Y-%m-%d')
     hora_actual = datetime.now()
     hora_actual_str = hora_actual.strftime('%H:%M:%S')
-    printer_ip = '192.168.8.35'
-    img_path = os.path.join(settings.STATIC_ROOT,'img', 'logo.png')
+    # printer_ip = '192.168.8.35'
+    # img_path = os.path.join(settings.STATIC_ROOT,'img', 'logo.png')
 
-    printer = Network(printer_ip)
-    printer.open()
-    printer.set(align='center')
-    printer.image(img_path, impl="bitImageColumn")
-    printer.text("\n")
+    # printer = Network(printer_ip)
+    # printer.open()
+    # printer.set(align='center')
+    # printer.image(img_path, impl="bitImageColumn")
+    # printer.text("\n")
 
-    printer.set(align='center', font='1', width=2, height=2, text_type='italic')
-    printer.text("Bienvenido")
-    printer.text("\n\n")
+    # printer.set(align='center', font='1', width=2, height=2, text_type='italic')
+    # printer.text("Bienvenido")
+    # printer.text("\n\n")
 
-    printer.set(align='center', font='1', width=5, height=5)
-    printer.text(""+codigo+"\n\n")
+    # printer.set(align='center', font='1', width=5, height=5)
+    # printer.text(""+codigo+"\n\n")
 
-    printer.set(align='center', width=1, height=1)
-    printer.text(departamento)
-    printer.text("\n\n")
+    # printer.set(align='center', width=1, height=1)
+    # printer.text(departamento)
+    # printer.text("\n\n")
 
-    printer.set(align='center', width=1, height=1)
-    printer.text("Gracias por su visita.")
-    printer.text("\n\n")
+    # printer.set(align='center', width=1, height=1)
+    # printer.text("Gracias por su visita.")
+    # printer.text("\n\n")
 
-    printer.set(align='center', width=1, height=1)
-    printer.text(fecha_actual+"  "+hora_actual_str)
-    printer.text("\n")
+    # printer.set(align='center', width=1, height=1)
+    # printer.text(fecha_actual+"  "+hora_actual_str)
+    # printer.text("\n")
 
     
 
-    printer.cut()
-    printer.close()
+    # printer.cut()
+    # printer.close()
 
     return True
